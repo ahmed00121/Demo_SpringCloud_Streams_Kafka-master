b68fb4b6f6a7f8bef88a3553ac5211ae41fe68aa
Ahmed Dahar <DAHAR_AHMED@emsi-edu.ma>
2023-01-23 16:53:22 +0100

    Initial commit

---
+++ src/test/java/com/ahmedjee/demospring_cloudstreams_kafka/DemoSpringCloudStreamsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmedjee.demospring_cloudstreams_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringCloudStreamsKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
